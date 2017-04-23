b25dfb29dbc73c69ee0a49070e192b1ae306dac6
schesnowitz <steve@chesnowitz.com>
2017-04-23 00:58:43 -0400

    test search console log

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react'; 
 import ReactDOM from 'react-dom'; 
 import SearchBar from './components/search_bar';
-
+import YTSearch from 'youtube-api-search';
 
 const API_Key = 'AIzaSyBAKHFMi21weYGvvaplraES6-HLZpqUmFU';
 
+
+YTSearch( {key: API_Key, term: 'Eric Clapton'}, function(data) { // test in console
+  console.log(data);
+});
 // const App = function() {
 // return <div>Hello Steve</div>;
 // };
