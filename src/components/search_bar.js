601dc0a63ad47e4c364ecffa4a8c683cde06cda7
schesnowitz <steve@chesnowitz.com>
2017-04-22 23:56:53 -0400

    SearchBar with event handler

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -11,10 +11,13 @@ const SearchBar = () => {
 
 class SearchBar extends Component {
     render() {
-			return(
-				<input />
-			);
+			return (
+				<input onChange={event => console.log(event.target.value)} /> // event handler gets triggered when even occurs
+				// onChange is react spacific property 
+				// no need to wrap event (event) => as only one argument
+				);
+			}
     }
-}
+
 
 export default SearchBar;
